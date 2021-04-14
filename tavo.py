217019b8a666c5cd45787dd3f9c138a96a5687e0
tavo1599 <>
2021-04-14 15:17:34 +0000

    operacionesas

--- tavo.py
+++ tavo.py
@@ -1,6 +1,10 @@
-<html>
-<header></header>
-<body>
-  <h1>Hola mundo</h1>
-</body>
-</html>git
+print ("Hola mundo/gustavo")
+a=45
+b=5
+print("suma:",a+b)
+print("resta:",a-b)
+print("division:",a/b)
+print("multiplicacion:",a*b)
+c=4
+d=5
+print("operacion:",c+d*c+c)
