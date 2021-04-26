c033939429d923b62beaf9c73909b01d973f9a31
tavo1599 <>
2021-04-26 15:05:51 +0000

    Lenguajes

---
+++ AreaTriangulo.kt
@@ -0,0 +1,12 @@
+fun main(Args: Array<String>){
+  var altura: Int
+  var base: Int
+  var area: Int
+  print("base: ")
+  base = readLine()!!.toInt()
+  print("altura: ")
+  altura = readLine()!!.toInt()
+  area = base*altura/2
+  print("area: ")
+  print(area)
+}
