12ed23bd7971a3c214ceb91d73d4a41ca2f6b050
tavo1599 <gustavo08_99@hotmail.com>
2021-05-17 09:15:58 -0500

    ejercicios

--- Repasojava.java
+++ Repasojava.java
@@ -1,50 +1,89 @@
 import java.util.Scanner;
 
+/**
+ * StrRepetitive
+ */
 public class Repasojava {
-    static Scanner objTecla1=new Scanner(System.in);
-    static int a=10;
-    static char b=8;
-    short c;
-    float d;
-    static double e=5;
-    long f;
-    String g="asdffgghj";
-    Integer j=54;
-    Double h=54.2;
-    Long m=(long)87;
 
-public static void saludo() {
-    System.out.println("Dime tu nombre: ");
-    String nombre=objTecla1.next();
-    System.out.println("Hola "+nombre);
-}
-
-public static void saludo2() {
-    System.out.println("Hola mundo");
-}
-public static void operacionesBasicas(){
-    System.out.println("Suma: "+(a+e));
-    System.out.println("Resta: "+(a-e));
-    System.out.println("Divicion: "+(a/e));
-    System.out.println("Multiplicacion: "+(a*e));
-    System.out.println("Potencia: "+(Math.pow(a, e)));
-}
-public void operacionesBasicas2(){
-    System.out.println("Suma: "+(a+e));
-    System.out.println("Resta: "+(a-e));
-    System.out.println("Divicion: "+(a/e));
-    System.out.println("Multiplicacion: "+(a*e));
-    System.out.println("Potencia: "+(Math.pow(a, e)));
-}
-
-public static void main(String[] args) {
-    // saludo2();
-    // saludo();
-    // operacionesBasicas();
-    Repasojava objRJ=new Repasojava();
-    objRJ.operacionesBasicas2();
-
-
+    static Scanner teclado=new Scanner(System.in);
     
-}
+
+    public static void saludo() {
+        System.out.println("Hola como estas?");
+    }
+    public static void name5() {
+        System.out.println("Probando");
+    }
+
+    public static void suma10NumerosMientras() {
+      //definir Variables
+        int contador=0;
+        double numeros, sumaNumeros=0;
+        //Datos de Entrada y proceso
+        while(contador<10){  //++contador    contador++
+            ++contador;
+            System.out.println("Ingrese el valor de la posicion "+contador+":");
+            numeros=teclado.nextDouble();
+            sumaNumeros=sumaNumeros+numeros;            
+        }
+        //Datos de salida
+        System.out.println("La suma de los 10 numeros es:"+sumaNumeros);        
+    }
+
+    public static void suma10NumerosHacerMientras() {
+        //definir Variables
+          int contador=0;
+          double numeros, sumaNumeros=0;
+          //Datos de Entrada y proceso
+         do{  //++contador:primero incrementa el valor     contador++: imprime el valor luego incrementa
+              ++contador;
+              System.out.println("Ingrese el valor de la posicion "+(contador)+":");
+              numeros=teclado.nextDouble();
+              sumaNumeros=sumaNumeros+numeros;
+              
+          } while(contador<10);
+          //Datos de salida
+          System.out.println("La suma de los 10 numeros es:"+sumaNumeros);        
+      }    
+        
+      public static void suma10NumerosParaHasta() {
+        //definir Variables
+          double numeros, sumaNumeros=0;
+          //Datos de Entrada y proceso
+          for(int contador=1;contador<=10; contador++){
+              System.out.println("Ingrese el valor de la posicion "+contador+":");
+              numeros=teclado.nextDouble();
+              sumaNumeros=sumaNumeros+numeros;              
+          }
+          //Datos de salida
+          System.out.println("La suma de los 10 numeros es:"+sumaNumeros);        
+      }       
+
+    public static void menuMain(){
+        String mensaje="Seleccion el algoritmo que desea ejecutar:"+
+        "\n1=suma10NumerosMientras"+
+        "\n2=suma10NumerosHacerMientras "+
+        "\n3=suma10NumerosPara"+
+        "\n0=Salir del programa";
+        
+        System.out.println(mensaje);
+        int opcion=0;
+        do{
+            opcion=teclado.nextInt();
+            switch(opcion){
+                case 1: suma10NumerosMientras();break;
+                case 2: suma10NumerosHacerMientras();;break;
+                case 3: suma10NumerosParaHasta();;break;
+                case 4: suma10NumerosMientras();break;
+            }
+            if(opcion!=0)
+            System.out.println("\n Desea seguir probando: "+mensaje);
+            
+        }while(opcion!=0);        
+    }
+    public static void main(String[] args) {
+    menuMain();
+    }
+
+
 }
